9e62b9abd12a08a7b525bd8d0c6bed089b12155f
Rilwan Gabi <rilwangabi10@gmail.com>
2025-11-08 16:50:27 +0100

    using ipinof for ip look up

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { GeofenceMiddleware } from './middleware/geofence.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule.forRoot({ isGlobal: true, cache: true })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/middleware/geofence.middleware.ts
+++ src/middleware/geofence.middleware.ts
@@ -1,12 +1,21 @@
 import { Injectable, NestMiddleware, ForbiddenException } from '@nestjs/common';
-import * as geoip from 'geoip-lite';
 import { Request, Response, NextFunction } from 'express';
 import { isPrivateIp } from 'src/common/helper';
 import { allowedCountries } from 'src/common/allowed-countries';
+import { IPinfoWrapper } from 'node-ipinfo';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class GeofenceMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
+  private ipinfo: IPinfoWrapper;
+
+  constructor(private readonly configService: ConfigService) {
+    this.ipinfo = new IPinfoWrapper(
+      this.configService.get<string>('IPINFO_TOKEN') as string,
+    );
+  }
+
+  async use(req: Request, res: Response, next: NextFunction) {
     const ip =
       (req.headers['x-forwarded-for'] as string)?.split(',')[0]?.trim() ||
       (req.headers['x-real-ip'] as string) ||
@@ -15,18 +24,26 @@ export class GeofenceMiddleware implements NestMiddleware {
 
     console.log('Resolved IP:', ip);
 
-    // Allow localhost and private/internal IPs (common in cloud setups)
+    //Allow localhost and private/internal IPs
     if (ip.includes('127.0.0.1') || ip.includes('::1') || isPrivateIp(ip)) {
       return next();
     }
+    try {
+      const response = await this.ipinfo.lookupIp(ip);
+      console.log('IPInfo response:', response);
 
-    const geo = geoip.lookup(ip);
+      const country = response.country;
 
-    if (!geo || !allowedCountries.includes(geo.country)) {
-      throw new ForbiddenException(
-        'Access restricted to UK, Canada, and Nigeria only.',
-      );
+      if (!country || !allowedCountries.includes(country)) {
+        throw new ForbiddenException(
+          'Access restricted to UK, Canada, and Nigeria only.',
+        );
+      }
+
+      next();
+    } catch (err) {
+      console.error('IPInfo lookup failed:', err);
+      throw new ForbiddenException('Unable to verify location.');
     }
-    next();
   }
 }
